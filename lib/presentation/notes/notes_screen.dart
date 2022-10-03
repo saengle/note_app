915232bb33a00d308e62515b1c57f0785c8379dd
saengle <zohv817@gmail.com>
2022-10-04 00:52:43 +0900

    Add: FirstScreen

---
+++ lib/presentation/notes/notes_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_note_app/domain/model/note.dart';
+import 'package:flutter_note_app/presentation/notes/components/note_item.dart';
+import 'package:flutter_note_app/ui/colors.dart';
+
+class NotesScreen extends StatelessWidget {
+  const NotesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Your Note',
+          style: TextStyle(fontSize: 30),
+        ),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.sort),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: ListView(
+        children: [
+          NoteItem(
+            note: Note(
+              title: 'title 1',
+              content: 'content 1',
+              color: wisteria.value,
+              timestamp: 1,
+            ),
+          ),
+          NoteItem(
+            note: Note(
+              title: 'title 2',
+              content: 'content 1',
+              color: skyBlue.value,
+              timestamp: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const Color roseBud = Color(0xFFFFAB91);
+const Color primrose = Color(0xFFE7ED9B);
+const Color wisteria = Color(0xFFCF94DA);
+const Color skyBlue = Color(0xFF81DEEA);
+const Color illusion = Color(0xFFF48FB1);
+
+const Color darkGray = Color(0xFF202020);
+const Color lightBlue = Color(0xFFd7E8DE);
