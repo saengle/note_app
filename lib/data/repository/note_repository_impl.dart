50e86e2653029afe0d37caa6364a47e1d77437c8
saengle <zohv817@gmail.com>
2022-10-03 20:00:24 +0900

    Add: sqflite

--- lib/data/repository/note_repository_impl.dart
+++ lib/data/repository/note_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter_note_app/data/data_source/note_db.dart';
 import 'package:flutter_note_app/domain/model/note.dart';
 import 'package:flutter_note_app/domain/repository/note_repository.dart';
@@ -8,32 +10,27 @@ class NoteRepositoryImpl implements NoteRepository {
   NoteRepositoryImpl(this.db);
 
   @override
-  Future<void> deleteNote(Note note) {
-    // TODO: implement deleteNote
-    throw UnimplementedError();
+  Future<void> deleteNote(Note note) async {
+    await db.deleteNote(note);
   }
 
   @override
-  Future<Note> getNoteById(int id) {
-    // TODO: implement getNoteById
-    throw UnimplementedError();
+  Future<Note?> getNoteById(int id) async {
+    return await db.getNoteById(id);
   }
 
   @override
-  Future<List<Note>> getNotes() {
-    // TODO: implement getNotes
-    throw UnimplementedError();
+  Future<List<Note>> getNotes() async {
+    return await db.getNotes();
   }
 
   @override
-  Future<void> insertNote(Note note) {
-    // TODO: implement insertNote
-    throw UnimplementedError();
+  Future<void> insertNote(Note note) async {
+    await db.insertNote(note);
   }
 
   @override
-  Future<void> updateNote(Note note) {
-    // TODO: implement updateNote
-    throw UnimplementedError();
+  Future<void> updateNote(Note note) async {
+    await db.updateNote(note);
   }
 }
